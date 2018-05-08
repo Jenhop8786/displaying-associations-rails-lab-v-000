c1a94dd3d02f57c90fde6f72808f7142c920911b
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-05-08 19:41:09 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,10 @@
 class ArtistsController < ApplicationController
   def index
-    @artist = Artist.all 
+    @artists = Artist.all 
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
